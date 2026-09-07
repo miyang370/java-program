5346cc89bcc7189f19addcfc0dab9e5b32584c54
miyang370 <miyang34@qq.com>
2026-09-07 20:51:56 +0800

    第二次提交

--- src/main/java/org/example/HelloWorld.java
+++ src/main/java/org/example/HelloWorld.java
@@ -2,6 +2,12 @@ package org.example;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int a = 2;
+        int b = 2;
+        int mark = 0 ;
+         int age = 3;
+         System.out.printf("%d,%d", age, mark);
+        System.out.println(a + b);
+        System.out.printf("%d  %d", a,b);
     }
 }
